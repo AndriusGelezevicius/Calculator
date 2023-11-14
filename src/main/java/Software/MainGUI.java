505fd178d04d4e47d74082dcdf051423e143cfa6
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2023-11-14 17:18:52 +0200

    add JTextField

--- src/main/java/Software/MainGUI.java
+++ src/main/java/Software/MainGUI.java
@@ -1,17 +1,47 @@
 package Software;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainGUI {
+public class MainGUI implements ActionListener {
         JFrame frame;
+        JTextField textField;
+        JButton[] numberButtons = new JButton[10];
+        JButton[] funcButtons = new JButton[8];
+        JButton addButton;
+        JButton subButton;
+        JButton multButton;
+        JButton divButton;
+        JButton decButton;
+        JButton equButton;
+        JButton delButton;
+        JButton clrButton;
+        JPanel panel;
+        double num1 = 0;
+        double num2 = 0;
+        double result = 0;
+        char operator;
 
     public void drawingFrame(){
             frame = new JFrame();
-            frame.setSize(300, 450);
+            frame.setSize(350, 550);
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
             frame.setLocationRelativeTo(null);
             frame.setLayout(null);
+
+            textField = new JTextField();
+            textField.setBounds(50,25,250,50);
+            textField.setEnabled(false);
+
+            frame.add(textField);
             frame.setVisible(true);
+
+
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
