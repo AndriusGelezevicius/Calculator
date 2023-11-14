a0937c45504e8040b40d224937d3c1bef4864343
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2023-11-14 18:39:15 +0200

    made buttons work. Added functionality

--- src/main/java/Software/MainGUI.java
+++ src/main/java/Software/MainGUI.java
@@ -99,5 +99,53 @@ public class MainGUI implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        for (int i = 0; i < 10; i++) {
+            if (e.getSource() == numberButtons[i]){
+                textField.setText(textField.getText().concat(String.valueOf(i)));
+            }
+        }
+        if(e.getSource() == decButton){
+            textField.setText(textField.getText().concat("."));
+        }
+        if(e.getSource() == addButton){
+            num1=Double.parseDouble(textField.getText());
+            operator = '+';
+            textField.setText("");
+        }
+        if(e.getSource() == subButton){
+            num1=Double.parseDouble(textField.getText());
+            operator = '-';
+            textField.setText("");
+        }
+        if(e.getSource() == multButton){
+            num1=Double.parseDouble(textField.getText());
+            operator = '*';
+            textField.setText("");
+        }
+        if(e.getSource() == divButton){
+            num1=Double.parseDouble(textField.getText());
+            operator = '/';
+            textField.setText("");
+        }
+        if(e.getSource() == equButton){
+            num2=Double.parseDouble(textField.getText());
+
+                switch(operator){
+                    case'+':
+                        result = num1+num2;
+                        break;
+                    case'-':
+                        result = num1-num2;
+                        break;
+                    case'*':
+                        result = num1*num2;
+                        break;
+                    case'/':
+                        result = num1/num2;
+                        break;
+                }
+                textField.setText(String.valueOf(result));
+                num1 = result;
+        }
     }
 }
